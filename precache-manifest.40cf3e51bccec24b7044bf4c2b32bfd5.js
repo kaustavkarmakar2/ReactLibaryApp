61485b0ba77e52bd7dad8d8aa848b42def7cc6ff
Kaustav Karmakar <kaustavkarmakar@Kaustavs-MacBook-Air.local>
2020-03-17 15:38:21 +0530

    Updates

---
+++ precache-manifest.40cf3e51bccec24b7044bf4c2b32bfd5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ebea07d428df8ffd926",
+    "url": "/kaustavkarmakar2.github.io/ReactLibaryApp/static/css/main.2f526613.chunk.css"
+  },
+  {
+    "revision": "4ebea07d428df8ffd926",
+    "url": "/kaustavkarmakar2.github.io/ReactLibaryApp/static/js/main.4ebea07d.chunk.js"
+  },
+  {
+    "revision": "f26ca547ea1825018188",
+    "url": "/kaustavkarmakar2.github.io/ReactLibaryApp/static/js/1.f26ca547.chunk.js"
+  },
+  {
+    "revision": "5e649f4485d8d923a56f",
+    "url": "/kaustavkarmakar2.github.io/ReactLibaryApp/static/js/runtime~main.5e649f44.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/kaustavkarmakar2.github.io/ReactLibaryApp/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/kaustavkarmakar2.github.io/ReactLibaryApp/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/kaustavkarmakar2.github.io/ReactLibaryApp/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "d16983cf71cb822d576f05122ce709c8",
+    "url": "/kaustavkarmakar2.github.io/ReactLibaryApp/index.html"
+  }
+];
